9ed7c39c6a17fcea56d9fcd1160ecfc60005f634
lobjakasa <aivar.lobjakas@cgi.com>
2015-11-23 22:24:03 +0200

    a

--- src/test/java/ExpiredLicenseSeleniumTest.java
+++ src/test/java/ExpiredLicenseSeleniumTest.java
@@ -28,7 +28,7 @@ public class ExpiredLicenseSeleniumTest {
     @Before
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
-        baseUrl = "ondemand.saucelabs.com:80/";
+        baseUrl = "localhost:4445";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
