9a14201f9a26058d94ca772d146b9a9ccb2634ac
lobjakasa <aivar.lobjakas@cgi.com>
2015-12-06 18:15:42 +0200

    conf

--- src/test/java/AddLicenseSeleniumTest.java
+++ src/test/java/AddLicenseSeleniumTest.java
@@ -23,9 +23,11 @@ public class AddLicenseSeleniumTest {
 
     @Before
     public void setUp() throws Exception {
-        driver = new FirefoxDriver();
-        baseUrl = System.getProperty("hostName");
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    	if(!baseUrl.equals("http://localhost:8080/")){
+    		driver = new FirefoxDriver();
+    		baseUrl = System.getProperty("hostName");
+    		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    	}
     }
 
     @Test
